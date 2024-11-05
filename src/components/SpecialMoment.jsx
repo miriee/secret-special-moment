391e0a0045565345c7ffa249029d780484ce82a1
“Mariem” <laouinimariem21@gmail.com>
2024-11-05 18:37:47 +0100

    fix: delete and add item

--- src/components/SpecialMoment.jsx
+++ src/components/SpecialMoment.jsx
@@ -2,42 +2,47 @@ import { useEffect, useState } from 'react';
 import { FloatingLabel, Form } from 'react-bootstrap';
 
 export const SpecialMoment = () => {
-    const [souvenir, setSouvenir] = useState({ note: '', date: '' });
-    const [souvenirs, setSouvenirs] = useState(JSON.parse(localStorage.getItem("liste")) || []);
-
-    useEffect(() => {
-        localStorage.setItem("liste", JSON.stringify(souvenirs));
-    }, [souvenirs]);
+    const [souvenir, setSouvenir] = useState({})
+    const [souvenirs, setSouvenirs] = useState(JSON.parse(localStorage.getItem("liste")) || [])
 
     const handleChange = ({ target }) => {
-        setSouvenir({ note: target.value, date: new Date() });
-    };
+        setSouvenir({ note: target.value, date: new Date() })
+    }
 
     const handleAdd = (event) => {
         event.preventDefault();
-        if (souvenir.note) { 
-            setSouvenirs((prev) => [...prev, souvenir]);
-            setSouvenir({ note: '', date: '' }); 
+        setSouvenirs(JSON.parse(localStorage.getItem("liste")) || [])
+        if (souvenir.note !== "") {
+            setSouvenirs((prev) => [...prev, souvenir])
+            setSouvenir({});
         }
         location.reload();
-    };
+    }
+
+    const addToLocalStorage = () => {
+        localStorage.setItem("liste", JSON.stringify(souvenirs));
+    }
+    useEffect(() => {
+        addToLocalStorage();
+    }, [souvenirs])
 
     return (
         <Form onSubmit={handleAdd}>
             <FloatingLabel controlId="floatingTextarea2" label="Écrivez un moment spécial...">
                 <Form.Control
                     as="textarea"
-                    placeholder="Laissez un commentaire ici"
+                    placeholder="Leave a comment here"
                     style={{ height: '200px' }}
                     onChange={handleChange}
-                    value={souvenir.note}
+                    value={souvenir.note || ""}
                 />
             </FloatingLabel>
             <button
-                style={{ backgroundColor: '#833b69', border: "none", marginTop: "3%", borderRadius: "3px", paddingBlock: '2%', paddingInline: '4%', color: "#30b8b4", fontWeight: "bold" }}   
+                style={{ backgroundColor: '#833b69', border: "none", marginTop: "3%", borderRadius: "3px", paddingBlock: '2%', paddingInline: '4%', color: "#30b8b4", fontWeight: "bold" }}
+                // onClick={handleAdd}
             >
                 Ajouter
             </button>
         </Form>
-    );
-};
+    )
+}
